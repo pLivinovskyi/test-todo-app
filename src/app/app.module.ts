85b6312c9025cead791b4ccfc9608468e75fbdb0
p-livinovskyi <p.livinovskyi@gmail.com>
2020-05-17 20:40:52 +0300

    generate missed router module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
